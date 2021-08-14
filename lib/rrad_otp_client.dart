2cfd76c902e2a7ba4c7e1012b9190c93a488f645
Tanvir Ahmed Khan <ftkhanft@Gmail.com>
2021-08-14 19:00:29 +0600

    Added send otp and macth otp

--- lib/rrad_otp_client.dart
+++ lib/rrad_otp_client.dart
@@ -1,1 +1,53 @@
 library rrad_otp_client;
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+class RRADOTP {
+  static final RRADOTP _singleton = RRADOTP._internal();
+  factory RRADOTP() {
+    return _singleton;
+  }
+  RRADOTP._internal();
+
+  initialize(ParseCoreData serverData) async {
+    await Parse().initialize(
+      serverData.applicationId,
+      serverData.serverUrl,
+      clientKey: serverData.clientKey,
+    );
+  }
+
+  Future<bool> sendOtp(String phoneNumber) async {
+    bool _otpStatus = false;
+    ParseCloudFunction _sendOtp = ParseCloudFunction('SendOtp');
+    ParseResponse response = await _sendOtp.execute(parameters: {
+      'phoneNumber': phoneNumber,
+    });
+    if (response.success && response.result != null) {
+      _otpStatus = response.result;
+    }
+    return Future.value(_otpStatus);
+  }
+
+  Future<bool> matchOtp(UserCredential userCredential) async {
+    bool _otpStatus = false;
+    ParseCloudFunction _sendOtp = ParseCloudFunction('MatchOtp');
+    ParseResponse response = await _sendOtp.execute(parameters: {
+      'phoneNumber': userCredential.phoneNumber,
+      'otp': userCredential.otp,
+    });
+    if (response.success && response.result != null) {
+      _otpStatus = response.result;
+    }
+    return Future.value(_otpStatus);
+  }
+}
+
+class UserCredential {
+  String phoneNumber;
+  String otp;
+  UserCredential({
+    required this.phoneNumber,
+    required this.otp,
+  });
+}
